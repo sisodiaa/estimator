c1baddab1390d41443009471f025d357b1ed0d7b
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2018-06-17 18:46:54 +0530

    Disable jbuilder

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,9 @@ module Estimator
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.jbuilder false
+    end
   end
 end

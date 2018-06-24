972c1de55170ee122728c3494ef75d0a91bdfa08
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2018-06-24 17:26:49 +0530

    Add scopes to model for complex queries

--- app/models/mill.rb
+++ app/models/mill.rb
@@ -1,13 +1,24 @@
 class Mill < ApplicationRecord
   include WhitespaceStripper
 
+  # Associations
   has_many :mill_machines, dependent: :destroy
   has_many :machines, through: :mill_machines
 
+
+  # Validations
   validates :name, presence: true
   validates :location, presence: true
   validates :code, presence: true, uniqueness: true
 
+
+  # Scopes
+  scope :with_machines_by_maker, lambda { |maker|
+    includes(:machines).where(machines: {make: maker})
+  }
+
+
+  # methods
   def to_param
     code
   end
--- app/models/mill_machine.rb
+++ app/models/mill_machine.rb
@@ -1,8 +1,24 @@
 class MillMachine < ApplicationRecord
+  # Associations
   belongs_to :mill
   belongs_to :machine
 
+
+  # Validations
   validates :quantity, presence: true,
     numericality: { only_integer: true, greater_than: 0 }
   validates :machine, presence: true
+
+
+  # Scopes
+  scope :count_of_machines_by_maker_in_a_mill, lambda { |mill, maker|
+    where(
+      mill: Mill.find_by(code: mill.code),
+      machine: Machine.where(make: maker)
+    ).sum(:quantity)
+  }
+  
+  scope :mills_with_machine_model, lambda { |model_id|
+    where(machine: Machine.where(id: model_id))
+  }
 end

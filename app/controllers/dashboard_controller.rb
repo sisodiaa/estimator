0676825f1110933ddc8efcbb0683c116fe24c72c
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2018-06-24 17:28:18 +0530

    Add dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   def index
   end
+
+  def show
+    response = QueryRunner.new(request.query_parameters).call
+    @result = QueryResult.new(response).call
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :machines
+
   resources :mills, param: :code do
     resources :mill_machines, as: 'machines', path: '/machines'
   end
+
+  get '/search', to: 'dashboard#show', as: 'search'
+
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

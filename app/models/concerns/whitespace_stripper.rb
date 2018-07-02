338dd5f80040a09c44d486eb71276333b18cb930
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2018-07-02 17:37:24 +0530

    Refactor and rectify whitespace stripper concern

--- app/models/concerns/whitespace_stripper.rb
+++ app/models/concerns/whitespace_stripper.rb
@@ -7,8 +7,8 @@ module WhitespaceStripper
 
   def strip_whitespace
     self.attribute_names.each do |attr_name|
-      if self[attr_name].respond_to?(:strip) && !self[attr_name].nil?
-        self.update_attribute(attr_name, self[attr_name].strip)
+      if self[attr_name] && self[attr_name].respond_to?(:strip)
+        self[attr_name] = self[attr_name].strip
       end
     end
   end

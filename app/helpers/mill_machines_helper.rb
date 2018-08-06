8a8e052480f54626160ed4109c0ab11eb3c23902
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2018-08-06 17:30:51 +0530

    Capitalize Machine Maker in machines list dropdown

--- app/helpers/mill_machines_helper.rb
+++ app/helpers/mill_machines_helper.rb
@@ -2,10 +2,10 @@ module MillMachinesHelper
   def machines_list
     hash = {}
     Machine.order(:make).order(:model).to_a.each do |machine|
-      if hash.has_key?(machine.make)
-        hash[machine.make].push([machine.model, machine.id])
+      if hash.has_key?(machine.make.upcase)
+        hash[machine.make.upcase].push([machine.model, machine.id])
       else
-        hash[machine.make] = [[machine.model, machine.id]]
+        hash[machine.make.upcase] = [[machine.model, machine.id]]
       end
     end
     hash.to_a
